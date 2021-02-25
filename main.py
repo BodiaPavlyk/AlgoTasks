eac6e215fea4c725f9dc029075efcdc72c9bdf62
BodiaPavlyk <bogdan2002mih@gmail.com>
2021-02-25 21:56:25 +0200

    task 2

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import math
 
 n = int(input('Input: '))
-print('r = ', math.floor(math.log(n, 2)) + 1)
-print('Result (2^r) = ', 2**(math.floor(math.log(n, 2)) + 1))
+
+for i in range(1, int(math.ceil(math.sqrt(n)))):
+    for j in range(1, int( math.ceil(math.sqrt(n-i**2)))):
+        third = n - i**2 - j**2
+        if third > 0 and float(third**(1/2))%1 == 0:
+            print(i, "^2 + ", j, "^2 + ", int(third**(1/2)), "^2")
+            exit(0)
+print("It`s impossible!")
