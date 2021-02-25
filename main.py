eab3f45dd204b15539c242ad646f7d40386e8f2a
BodiaPavlyk <bogdan2002mih@gmail.com>
2021-02-25 21:58:10 +0200

    task 3

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import math
 
 n = int(input('Input: '))
 
+exist = False
 for i in range(1, int(math.ceil(math.sqrt(n)))):
-    for j in range(1, int( math.ceil(math.sqrt(n-i**2)))):
+    for j in range(1, int(math.ceil(math.sqrt(n)))):
         third = n - i**2 - j**2
         if third > 0 and float(third**(1/2))%1 == 0:
+            exist = True
             print(i, "^2 + ", j, "^2 + ", int(third**(1/2)), "^2")
-            exit(0)
-print("It`s impossible!")
+if not exist:
+    print("It`s impossible!")
