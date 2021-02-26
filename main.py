7539a65e31f7d69a05303b72345ecbc4e5f4d043
Bodia <55597251+BodiaPavlyk@users.noreply.github.com>
2021-02-26 12:02:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
+from math import floor, log
 
 n = int(input('Input: '))
-print('r = ', math.floor(math.log(n, 2)) + 1)
-print('Result (2^r) = ', 2**(math.floor(math.log(n, 2)) + 1))
+print('r = ', floor(log(n, 2)) + 1)
+print('Result (2^r) = ', 2**(floor(log(n, 2)) + 1))
