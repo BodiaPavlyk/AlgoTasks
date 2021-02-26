b186d1cbe3982e19ffe8ee7d6a89ed6b3df8f303
Bodia <55597251+BodiaPavlyk@users.noreply.github.com>
2021-02-26 12:16:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import math
+from math import ceil, sqrt
+
+def check(number):
+  exist = False
+  for i in range(1, int(ceil(sqrt(number)))):
+      for j in range(1, int(ceil(sqrt(number-i**2)))):
+        third = number - i**2 - j**2
+        if third > 0 and float(third**(1/2))%1 == 0:
+          exist = True
+          print(i, "^2 + ", j, "^2 + ", int(third**(1/2)), "^2")
+  if not exist:
+      return False
+  else:
+      return True
+
 
 n = int(input('Input: '))
-
-exist = False
-for i in range(1, int(math.ceil(math.sqrt(n)))):
-    for j in range(1, int(math.ceil(math.sqrt(n)))):
-        third = n - i**2 - j**2
-        if third > 0 and float(third**(1/2))%1 == 0:
-            exist = True
-            print(i, "^2 + ", j, "^2 + ", int(third**(1/2)), "^2")
-if not exist:
-    print("It`s impossible!")
+if not check(n):
+  print("It`s impossible!")
